08255afd71dbb6b82ae287c2643deee022e3a5ed
Yena-Yun <cheryleduart@gmail.com>
2022-05-04 12:53:50 +0900

    react-query 1강-8강

--- react-query-starter/src/components/RQSuperHeroes.page.js
+++ react-query-starter/src/components/RQSuperHeroes.page.js
@@ -1,3 +1,36 @@
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
+const fetchSuperHeroes = () => {
+  return axios.get('http://localhost:4000/superheroes');
+};
+
 export const RQSuperHeroesPage = () => {
-  return <h2>React Query Super Heroes Page</h2>
-}
+  const { isLoading, data, isError, error, isFetching } = useQuery(
+    'super-heroes',
+    fetchSuperHeroes,
+    {
+      refetchOnMount: true,
+      refetchOnWindowFocus: 'always',
+    }
+  );
+
+  console.log({ isLoading, isFetching });
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (isError) {
+    return <h2>{error.message}</h2>;
+  }
+
+  return (
+    <>
+      <h2>React Query Super Heroes Page</h2>
+      {data?.data.map((hero) => {
+        return <div key={hero.name}>{hero.name}</div>;
+      })}
+    </>
+  );
+};
